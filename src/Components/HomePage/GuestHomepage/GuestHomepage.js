d4d1924dfddbc31b85b738bce2bcb7a6c6a9621c
Anshul2166 <anshuldubey2166@gmail.com>
2018-05-28 18:49:15 +0530

    Feature rows done

--- src/Components/HomePage/GuestHomepage/GuestHomepage.js
+++ src/Components/HomePage/GuestHomepage/GuestHomepage.js
@@ -1,45 +1,98 @@
-import React, {Component} from "react";
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
+import React, { Component } from "react";
+import Grid from "@material-ui/core/Grid";
 import logo from "../../Common/Images/Background.png";
 import "./GuestHomepage.css";
 
-class GuestHomepage extends Component{
+const data = {
+	details: [
+		{
+			heading: "A world that works for you",
+			content:
+				"IFTTT is the free way to get all your apps and devices talking to each other. Not everything on the internet plays nice, so we're on a mission to build a more connected world.",
+			alt: "N"
+		},
+		{
+			heading: "Post your photo anywhere and see it everywhere",
+			content:
+				"We help social media play nice together. Never tweet a link instead of a picture again!",
+			alt: "Y"
+		},
+		{
+			heading: "A world that works for you",
+			content:
+				"IFTTT is the free way to get all your apps and devices talking to each other. Not everything on the internet plays nice, so we're on a mission to build a more connected world.",
+			alt: "N"
+		},
+		{
+			heading: "Make your voice assistant more personal",
+			content:
+				"Just say the word, and we’ll help you stay in-sync across systems as you go through your day.",
+			alt: "Y"
+		}
+	]
+};
+
+class GuestHomepage extends Component {
 	constructor(props) {
-	  super(props);
-	  this.state = {};
+		super(props);
+		this.state = {};
 	}
-	render(){
-		return(
-			<div className="guest-homepage">
-				<FeatureRow />
-			</div>
-		);
+	render() {
+		const rowsList = data.details.map(contents => (
+			<FeatureRow
+				heading={contents.heading}
+				content={contents.content}
+				alt={contents.alt}
+			/>
+		));
+		return <div className="guest-homepage">{rowsList}</div>;
 	}
 }
 
-class FeatureRow extends Component{
+class FeatureRow extends Component {
 	constructor(props) {
-	  super(props);
-	
-	  this.state = {};
+		super(props);
+
+		this.state = {};
 	}
-	render(){
-		return(
-		<div className="feature-row">
-			<Grid container spacing={24}>
+	render() {
+		const alt = this.props.alt;
+		const FirstContent =
+			alt == "Y" ? (
+				<Grid item sm={5}>
+					<img src={logo} className="logo" />
+				</Grid>
+			) : (
 				<Grid item sm={4}>
-					<h1>A world that works for you</h1>
-					<p>IFTTT is the free way to get all your apps and devices talking to each other. Not everything on the internet plays nice, so we're on a mission to build a more connected world.</p>
+					<div className="text">
+						<h1>{this.props.heading}</h1>
+						<p>{this.props.content}</p>
+					</div>
 				</Grid>
-				<Grid item sm={2}>
+			);
+		const SecondContent =
+			alt == "Y" ? (
+				<Grid item sm={5}>
+					<div className="text">
+						<h1>{this.props.heading}</h1>
+						<p>{this.props.content}</p>
+					</div>
 				</Grid>
+			) : (
 				<Grid item sm={6}>
-					<img src={logo} className="logo"/>
+					<img src={logo} className="logo" />
 				</Grid>
-			</Grid>
-		</div>
+			);
+		return (
+			<div className="feature-row">
+				<Grid container spacing={24}>
+					{FirstContent}
+
+					<Grid item sm={2} />
+
+					{SecondContent}
+				</Grid>
+			</div>
 		);
 	}
 }
