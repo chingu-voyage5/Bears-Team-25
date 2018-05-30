df6a33dccd9fd41f5e7e135b6724f9a8eb5fd19e
Anshul2166 <anshuldubey2166@gmail.com>
2018-05-30 23:45:10 +0530

    Rendering code from Service-card now for step-1

---
+++ src/Components/CreateApplet/ServiceCard/ServiceCard.js
@@ -0,0 +1,52 @@
+import React,{Component} from "react";
+import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
+import Card from "@material-ui/core/Card";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import logo from "../../Common/Images/Background.png";
+import FontAwesomeIcon from "@fortawesome/react-fontawesome";
+import faCoffee from "@fortawesome/fontawesome-free-solid/faCoffee";
+
+class ServiceCard extends Component{
+	constructor(props) {
+	  super(props);
+	  this.state = {};
+	}
+	render(){
+		const ServiceList=this.props.json;
+
+		const createServiceList = ServiceList.list.map(service => (
+			<Grid item sm={3}>
+				<Card className="card" style={{backgroundColor:service.color}}>
+					<div className="text-center media-icon">
+						<CardMedia>
+							<FontAwesomeIcon icon={faCoffee} size="6x" style={{color:"#fff"}}/>
+						</CardMedia>
+					</div>
+					<CardContent>
+						<Typography
+							gutterBottom
+							variant="headline"
+							className="text-center"
+							component="h2"
+							style={{color:"#fff"}}
+						>
+							{service.name}
+						</Typography>
+					</CardContent>
+				</Card>
+			</Grid>
+		));
+	
+		return(
+			<div className="service-card">
+				<Grid container spacing={24}>
+				{createServiceList}
+				</Grid>
+			</div>
+		);
+	}
+}
+export default ServiceCard;
--- src/Components/CreateApplet/Step1/Step1.js
+++ src/Components/CreateApplet/Step1/Step1.js
@@ -1,13 +1,6 @@
 import React, { Component } from "react";
 import Grid from "@material-ui/core/Grid";
-import Button from "@material-ui/core/Button";
-import Card from "@material-ui/core/Card";
-import CardMedia from "@material-ui/core/CardMedia";
-import CardContent from "@material-ui/core/CardContent";
-import Typography from "@material-ui/core/Typography";
-import logo from "../../Common/Images/Background.png";
-import FontAwesomeIcon from "@fortawesome/react-fontawesome";
-import faCoffee from "@fortawesome/fontawesome-free-solid/faCoffee";
+import ServiceCard from "../ServiceCard/ServiceCard";
 import "./Step1.css";
 
 class Step1 extends Component {
@@ -41,28 +34,6 @@ class Step1 extends Component {
 			]
 		};
 
-		const createServiceList = ServiceList.list.map(service => (
-			<Grid item sm={3}>
-				<Card className="card" style={{backgroundColor:service.color}}>
-					<div className="text-center media-icon">
-						<CardMedia>
-							<FontAwesomeIcon icon={faCoffee} size="6x" style={{color:"#fff"}}/>
-						</CardMedia>
-					</div>
-					<CardContent>
-						<Typography
-							gutterBottom
-							variant="headline"
-							className="text-center"
-							component="h2"
-							style={{color:"#fff"}}
-						>
-							{service.name}
-						</Typography>
-					</CardContent>
-				</Card>
-			</Grid>
-		));
 		return (
 			<div className="step">
 				<div className="text-center">
@@ -71,9 +42,9 @@ class Step1 extends Component {
 				<div className="text-center">
 					<p>Step 1 of 6</p>
 				</div>
-				<Grid container spacing={24}>
-					{createServiceList}
-				</Grid>
+				
+					<ServiceCard json={ServiceList}/>
+				
 			</div>
 		);
 	}
