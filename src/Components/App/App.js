5e101ee4aad88c675d05ba9d444d29cd6fecfdd0
Anshul2166 <anshuldubey2166@gmail.com>
2018-05-30 16:36:52 +0530

    Added react-router to project and added routes

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,15 +1,20 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Switch, Route } from 'react-router-dom'
 import Homepage from "../Homepage/Homepage";
-import './App.css';
+import CreateApplet from "../CreateApplet/CreateApplet";
+import "./App.css";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Homepage />
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<Switch>
+					<Route exact path="/" component={Homepage} />
+					<Route path="/createapplet" component={CreateApplet} />
+				</Switch>
+			</div>
+		);
+	}
 }
 
 export default App;
