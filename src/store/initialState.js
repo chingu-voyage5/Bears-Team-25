2666d1466c5486962407d90a53fe268fafe57b11
Anshul2166 <anshuldubey2166@gmail.com>
2018-06-08 19:13:19 +0530

    Added triggers for Fb,Twitter,Insta

--- src/store/initialState.js
+++ src/store/initialState.js
@@ -65,6 +65,58 @@ const initialState = {
 				content:
 					"This trigger fires every time a card is added to a board"
 			}
+		],
+		Facebook: [
+			{
+				heading: "New photo post by you with hashtag",
+				content:
+					"This Trigger fires every time you post a new photo on Facebook with a specific hashtag."
+			},
+			{
+				heading: "New status message by you with hashtag",
+				content:
+					"This Trigger fires every time you create a new plain text status message on Facebook with a specific hashtag."
+			},
+			{
+				heading: "New link post by you with hashtag",
+				content:
+					"This Trigger fires every time you create a new link post on Facebook with a specific hashtag."
+			}
+		],
+		Twitter: [
+			{
+				heading: "New tweet by you",
+				content: "This trigger fires everytime you post a new tweet"
+			},
+			{
+				heading: "New mention of you",
+				content: "This trigger fires everytime when you are mentioned"
+			},
+			{
+				heading: "New liked tweet by you",
+				content: "This trigger fires everytime when you like a tweet"
+			},
+			{
+				heading: "New tweet by you",
+				content: "This trigger fires everytime you post a new tweet"
+			}
+		],
+		Instagram: [
+			{
+				heading: "Add new photo by you",
+				content:
+					"This trigger fires when you share any new photo on Instagram"
+			},
+			{
+				heading: "New photo by you using specific hashtag",
+				content:
+					"This trigger fires when you share any new photo on Instagram, with a hashtag you supplied"
+			},
+			{
+				heading: "Any new video by you",
+				content:
+					"This trigger fires everytime when you post a video on Instagram"
+			}
 		]
 	},
 	action: {
