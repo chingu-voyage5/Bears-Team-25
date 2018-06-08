bbc842227fd062a4288738cd9ca44d7a2cae98ab
Anshul2166 <anshuldubey2166@gmail.com>
2018-06-08 15:54:19 +0530

    Added and styled the footer

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom'
 import Homepage from "../Homepage/Homepage";
 import CreateApplet from "../CreateApplet/CreateApplet";
 import MyApplet from "../MyApplet/MyApplet";
+import Footer from "../Footer/Footer";
 import "./App.css";
 
 class App extends Component {
@@ -14,6 +15,7 @@ class App extends Component {
 					<Route path="/createapplet" component={CreateApplet} />
 					<Route path="/myapplets" component={MyApplet} />
 				</Switch>
+				<Footer />
 			</div>
 		);
 	}
---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,63 @@
+/**
+ * This component have two columns.
+ * First column just show company logo
+ * Rest all columns (.footer_links) contain all kind of links which have been seperated into another 4 columns.
+ **/
+import { Link } from "react-router-dom";
+import React, { Component } from "react";
+import logo from "../Common/Images/ifttt-banner.png";
+import "./Footer.css";
+
+class Footer extends Component {
+  render() {
+    return (
+      <div className="footer-overlay">
+        <div className="footer">
+          <div className="footer__logo1">
+              <img src={logo} alt="Project Showcase" />
+          </div>
+          <div className="footer__links">
+            <div className="footer__links__column">
+              <div>CONNECT</div>
+              <ul>
+                <li>Help</li>
+                <li>Facebook</li>
+                <li>Github</li>
+                <li>Medium</li>
+              </ul>
+            </div>
+            <div className="footer__links__column">
+              <div>GENERAL</div>
+              <ul>
+                <li>About</li>
+                <li>Blogs</li>
+                <li>Careers</li>
+                <li>Contact</li>
+              </ul>
+            </div>
+            <div className="footer__links__column">
+              <div>SPECIALTY PAGES</div>
+              <ul>
+                <li>Most Viewed</li>
+                <li>High Rated</li>
+                <li>Recommended Projects</li>
+                <li>New Additions</li>
+              </ul>
+            </div>
+            <div className="footer__links__column">
+              <div>ACCOUNTS</div>
+              <ul>
+                <li>My Account</li>
+                <li>Community</li>
+                <li>Github</li>
+                <li>chingu.io</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Footer;
