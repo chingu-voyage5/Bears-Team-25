e3e64a5c1a6fb997c6c7be3f949c51a16dcfa1fe
Ruslan <4ruslan.k@gmail.com>
2018-06-10 01:59:52 +0500

    mongodb

--- server/app.js
+++ server/app.js
@@ -5,17 +5,30 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 var appRoot = require('app-root-path');
 var favicon = require('serve-favicon');
+const mongoose = require('mongoose');
+var passport = require('passport');
+var session = require('express-session');
+require('dotenv').load(); // loading .env file
 
 const index = require('./routes/index');
 
 const app = express();
+
+// Connect TO MongoDB though Mlab. Commented out till shared account for team created 
+
+// mongoose.connect(process.env.URI);
+// var db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'connection error:'));
+// db.once('open', function () {
+//   console.log('connected to mongoDB');
+// });
 
 app.use(favicon(path.join(appRoot.path, 'build/favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(appRoot.path,'build')));
+app.use(express.static(path.join(appRoot.path, 'build')));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -27,14 +40,14 @@ app.get('*', (req, res) => {
 });
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
