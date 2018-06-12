8110e1d596a6eaff63f8ad5e8dae7a41be7adc30
Anshul2166 <anshuldubey2166@gmail.com>
2018-06-12 23:09:25 +0530

    Declared model for applet

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,9 +3,10 @@ var router = express.Router();
 
 // subroutes
 const users = require('./users');
-
+const applets=require('./applets');
 // paths to subroutes (`api/${path}`)
 router.use('/users', users)
+router.use('/applets', applets)
 
 
 module.exports = router;
