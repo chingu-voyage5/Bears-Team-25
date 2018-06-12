76486a8b790dccaa62bb56a524dc34118c352fb1
Ruslan <4ruslan.k@gmail.com>
2018-06-12 18:36:28 +0500

    mock-api with axio

--- src/actions/profileSettingsActions.js
+++ src/actions/profileSettingsActions.js
@@ -2,84 +2,69 @@ import * as ACTIONS from "./actionTypes";
 const axios = require("axios");
 
 function change_email_on() {
-    return {
-        type: ACTIONS.CHANGING_EMAIL
-    }
+  return {
+    type: ACTIONS.CHANGING_EMAIL
+  };
 }
-
 
 function change_email_success(json) {
-    return {
-        type: ACTIONS.CHANGING_EMAIL_SUCCESS
-    }
+  return {
+    type: ACTIONS.CHANGING_EMAIL_SUCCESS
+  };
 }
 
-
-
 function change_email_failure(error) {
-    return {
-        type: ACTIONS.CHANGING_EMAIL_FAILURE,
-        error: error
-    }
+  return {
+    type: ACTIONS.CHANGING_EMAIL_FAILURE,
+    error: error
+  };
 }
 
 function change_email_success_snackbar() {
-    return {
-        type: ACTIONS.RENDER_SNACKBAR,
-        styling: 'success',
-        text: "You've successfully changed email!"
-    }
+  return {
+    type: ACTIONS.RENDER_SNACKBAR,
+    styling: "success",
+    text: "You've successfully changed email!"
+  };
 }
 
 function change_email_failure_snackbar(error) {
-    return {
-        type: ACTIONS.RENDER_SNACKBAR,
-        styling: 'error',
-        text: error
-    }
+  return {
+    type: ACTIONS.RENDER_SNACKBAR,
+    styling: "error",
+    text: error
+  };
 }
 
 export function linkOrUnlinkFB() {
-    return {
-        type: ACTIONS.LINK_FB,
-    }
+  return {
+    type: ACTIONS.LINK_FB
+  };
 }
 
 export function linkOrUnlinkGoogle() {
-    return {
-        type: ACTIONS.LINK_GOOGLE,
-    }
+  return {
+    type: ACTIONS.LINK_GOOGLE
+  };
 }
 
-
-
 export function change_email(values) {
-
-    return function (dispatch) {
-
-        // First dispatch: the app state is updated to inform
-        dispatch(change_email_on());
-
-        fetch('https://reqres.in/api/users', {
-            method: "post",
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                email: values.email
-            })
-        })
-            .then(
-                json => {
-                    dispatch(change_email_success(json))
-                    dispatch(change_email_success_snackbar())
-                },
-                error => {
-                    console.log(error);
-                    dispatch(change_email_failure(error))
-                    dispatch(change_email_failure_snackbar(error))
-                }
-            )
-    }
+  console.log(values);
+  return function(dispatch) {
+    // First dispatch: the app state is updated to inform
+    dispatch(change_email_on());
+    axios
+      .post("https://reqres.in/api/users", {
+        email: values.email
+      })
+      .then(() => {
+        dispatch(change_email_success());
+        dispatch(change_email_success_snackbar());
+      })
+      .catch(error => {
+        error = error.response.data.status;
+        dispatch(change_email_failure(error));
+        dispatch(change_email_failure_snackbar(error));
+      });
+  };
 }
