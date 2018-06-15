6a094d1c9de893c66fca85ec5ab76884e7615497
Ruslan <4ruslan.k@gmail.com>
2018-06-15 20:40:45 +0500

    transfer fetchUsers from HomePage to Header

--- src/Components/HomePage/Homepage.js
+++ src/Components/HomePage/Homepage.js
@@ -2,14 +2,9 @@ import React,{Component} from 'react';
 import LoggedUserHomePage from "./LoggedUserHomePage/LoggedUserHomePage";
 import GuestHomepage from "./GuestHomepage/GuestHomepage";
 import { connect } from 'react-redux';
-import {fetchUsersCredentials} from '../../actions/loginActions';
+
 
 class Homepage extends Component{
-	componentDidMount() {
-		const {fetchUsersCredentials} = this.props;
-		fetchUsersCredentials();
-	}
-
 	render(){
 		const {auth}=this.props
 		const Page=auth?(<LoggedUserHomePage />):(<GuestHomepage />)
@@ -27,4 +22,4 @@ function mapStateToProps(state) {
     }
 }
 
-export default connect(mapStateToProps, {fetchUsersCredentials})(Homepage);
+export default connect(mapStateToProps)(Homepage);
