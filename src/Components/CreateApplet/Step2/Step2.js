3ff95cf847892f967794e6e246b2eb5b844184f4
Anshul2166 <anshuldubey2166@gmail.com>
2018-06-29 14:16:44 +0530

    Undefined avoided in triggerlist

--- src/Components/CreateApplet/Step2/Step2.js
+++ src/Components/CreateApplet/Step2/Step2.js
@@ -18,6 +18,10 @@ class Step2 extends Component{
 		const service=this.props.service;
 		console.log(service); 
 		const TriggerList = this.props.TriggerList[service];
+		if(TriggerList===undefined){
+			console.log("undefined list");
+			return null;
+		}
 		if (this.props.currentStep !== 2) {
 			return null;
 		}
--- src/store/initialState.js
+++ src/store/initialState.js
@@ -1,199 +1,33 @@
 const initialState = {
 	service: {
-		allServiceList:[
-		"Mail","Trello","Github","Facebook","Twitter","Instagram","Slack"
-	],
-	serviceList:[]
-},
+		allServiceList: [
+			"Mail",
+			"Trello",
+			"Github",
+			"Facebook",
+			"Twitter",
+			"Instagram",
+			"Slack"
+		],
+		serviceList: []
+	},
 	trigger: {
-		Trello: [
-			{
-				heading: "Card added to board",
-				content:
-					"This trigger fires every time a card is added to a board"
-			},
-			{
-				heading: "Card added to board",
-				content:
-					"This trigger fires every time a card is added to a board"
-			},
-			{
-				heading: "Card added to board",
-				content:
-					"This trigger fires every time a card is added to a board"
-			},
-			{
-				heading: "Card added to board",
-				content:
-					"This trigger fires every time a card is added to a board"
-			},
-			{
-				heading: "Card added to board",
-				content:
-					"This trigger fires every time a card is added to a board"
-			},
-			{
-				heading: "Card added to board",
-				content:
-					"This trigger fires every time a card is added to a board"
-			}
-		],
-		Facebook: [
-			{
-				heading: "New photo post by you with hashtag",
-				content:
-					"This Trigger fires every time you post a new photo on Facebook with a specific hashtag."
-			},
-			{
-				heading: "New status message by you with hashtag",
-				content:
-					"This Trigger fires every time you create a new plain text status message on Facebook with a specific hashtag."
-			},
-			{
-				heading: "New link post by you with hashtag",
-				content:
-					"This Trigger fires every time you create a new link post on Facebook with a specific hashtag."
-			}
-		],
-		Twitter: [
-			{
-				heading: "New tweet by you",
-				content: "This trigger fires everytime you post a new tweet"
-			},
-			{
-				heading: "New mention of you",
-				content: "This trigger fires everytime when you are mentioned"
-			},
-			{
-				heading: "New liked tweet by you",
-				content: "This trigger fires everytime when you like a tweet"
-			},
-			{
-				heading: "New tweet by you",
-				content: "This trigger fires everytime you post a new tweet"
-			}
-		],
-		Instagram: [
-			{
-				heading: "Add new photo by you",
-				content:
-					"This trigger fires when you share any new photo on Instagram"
-			},
-			{
-				heading: "New photo by you using specific hashtag",
-				content:
-					"This trigger fires when you share any new photo on Instagram, with a hashtag you supplied"
-			},
-			{
-				heading: "Any new video by you",
-				content:
-					"This trigger fires everytime when you post a video on Instagram"
-			}
-		],
-		Github: [
-			{
-				heading: "Any new issue",
-				content:
-					"This trigger fires every time any new issue is opened in a repository you own or collabrate with"
-			},
-			{
-				heading: "New Pull request for specified repository",
-				content:
-					"This Trigger fires everytime a pull request is opened for a repository you specify"
-			}
-		]
+		Trello: [],
+		Facebook: [],
+		Twitter: [],
+		Instagram: [],
+		Github: [],
+		Slack: [],
+		Mail: []
 	},
 	action: {
-		Mail: [
-			{
-				heading: "Send me an email",
-				content:
-					"This Action will send you an HTML based email. Images and links are supported."
-			}
-		],
-		Twitter: [
-			{
-				heading: "Post an image with tweet",
-				content: "This action will tweet along with linked image"
-			},
-			{
-				heading: "Post a tweet",
-				content: "This Action will post a new tweet"
-			}
-		],
-		Facebook: [
-			{
-				heading: "Create a status message",
-				content: "This action will add a plain text status on Facebook"
-			},
-			{
-				heading: "Create a link post",
-				content: "This action will create new link post on Facebook"
-			}
-		],
-		Github: [
-			{
-				heading: "Create an issue",
-				content:
-					"This action will create an issue in your specified repository"
-			}
-		]
+		Mail: [],
+		Twitter: [],
+		Facebook: [],
+		Github: []
 	},
 	create: {
-		appletList: [
-			{
-				actionHeading: "Send me an email",
-				actionContent:
-					"This Action will send you an HTML based email. Images and links are supported.",
-				content: "Random content",
-				heading: "Random content",
-				triggerHeading: "Add a Card to trello",
-				triggerContent:
-					"A card has been added to trello board of yours",
-				serviceFrom: "Trello",
-				serviceTo: "Mail",
-				condition: "On"
-			},
-			{
-				actionHeading: "Send me an email",
-				actionContent:
-					"This Action will send you an HTML based email. Images and links are supported.",
-				content: "Random content",
-				heading: "Random content",
-				triggerHeading: "Add a Card to trello",
-				triggerContent:
-					"A card has been added to trello board of yours",
-				serviceFrom: "Trello",
-				serviceTo: "Mail",
-				condition: "On"
-			},
-			{
-				actionHeading: "Send me an email",
-				actionContent:
-					"This Action will send you an HTML based email. Images and links are supported.",
-				content: "Random content",
-				heading: "Random content",
-				triggerHeading: "Add a Card to trello",
-				triggerContent:
-					"A card has been added to trello board of yours",
-				serviceFrom: "Trello",
-				serviceTo: "Mail",
-				condition: "On"
-			},
-			{
-				actionHeading: "Send me an email",
-				actionContent:
-					"This Action will send you an HTML based email. Images and links are supported.",
-				content: "Random content",
-				heading: "Random content",
-				triggerHeading: "Add a Card to trello",
-				triggerContent:
-					"A card has been added to trello board of yours",
-				serviceFrom: "Trello",
-				serviceTo: "Mail",
-				condition: "On"
-			}
-		]
+		appletList: []
 	},
 	activity: {
 		activityList: []
@@ -205,4 +39,3 @@ const initialState = {
 };
 
 export default initialState;
-
