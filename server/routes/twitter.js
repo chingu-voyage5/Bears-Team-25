99bddce7c2cbab52d44b9c03d9cfd9f6a4584d24
Ruslan <4ruslan.k@gmail.com>
2018-07-09 01:51:41 +0500

    twitter any tweets / hashtages tweets

--- server/routes/twitter.js
+++ server/routes/twitter.js
@@ -5,6 +5,45 @@ var passport = require("passport");
 var isLoggedIn = require('../commonFunctions').isLoggedIn;
 var User = require("../models/users");
 var addToNotSubscribedRemoveFromSubscribed = require('../commonFunctions').addToNotSubscribedRemoveFromSubscribed;
+var postTrelloCard = require("../routes/trello").postCard;
+var slackSendMessage = require("../routes/slack").slackSendMessage;
+var transporter = require('../routes/email').transporter;
+var mailOptions = require('../routes/email').mailOptions;
+
+
+//trello actions
+function trelloActions (applets, trelloToken, cardTitle, description) {
+	appletsWithTrelloActions = applets.filter(applet => applet.option.watchTo === 'Trello');
+	for (appletsWithTrelloAction of appletsWithTrelloActions) {
+		let trelloOptions = appletsWithTrelloAction.action.trelloOptions;
+		let trelloConfig = {...trelloOptions, token: trelloToken, cardTitle, description}
+		postTrelloCard(trelloConfig).then(card => {
+			console.log("trello card posted");
+		});
+	}
+}
+
+function slackActions(applets, slackToken, message) {
+	appletsWithSlackActions = applets.filter(applet => applet.option.watchTo === 'Slack');
+	for (appletsWithSlackAction of appletsWithSlackActions) {
+		let slackOptions = appletsWithSlackAction.action.slackOptions;
+		slackSendMessage(slackToken, message, slackOptions.to).then(message => {
+			console.log("slack message sent");
+		});
+	}
+}
+
+function mailActions (applets, message, user) {
+	appletsWithMailActions = applets.filter(applet => applet.option.watchTo === 'Mail');
+	for (appletsWithMailAction of appletsWithMailActions) {
+		let options = mailOptions(user, {email:  appletsWithMailAction.action.mailOptions.email,
+				message: message})
+		transporter.sendMail(options).then(message => {
+			console.log("email  sent");
+		});
+	}
+}
+
 
 twitterRouter.get("/auth", passport.authorize("twitter"));
 
@@ -33,7 +72,9 @@ twitterRouter.get("/disconnect", isLoggedIn, (req, res, next) => {
 		return next(err);
 	  }
 	);
-  });
+	});
+	
+	var streams = {}
 
 	User.find({ 'twitter.id': { $exists: true } })
 	.exec(function(err, users) {
@@ -42,6 +83,8 @@ twitterRouter.get("/disconnect", isLoggedIn, (req, res, next) => {
 		consumer_secret = JSON.parse(process.env.twitter).consumerSecret;
 		if (users.length !== 0) {
 			for (let user of users) {
+				let id = user.twitter.id;
+				if (streams[id]) continue
 				var T = new Twit({
 					consumer_key,
 					consumer_secret,
@@ -49,22 +92,75 @@ twitterRouter.get("/disconnect", isLoggedIn, (req, res, next) => {
 					access_token_secret: user.twitter.tokenSecret,
 					timeout_ms: 60 * 1000, // optional HTTP request timeout to apply to all requests.
 				});
+				
 
 				var stream = T.stream('user');
+				streams[id] = stream;
 
 				stream.on('tweet', function (tweet) {
-					console.log(tweet);
-					// twit.text
-					// twit.id
-					// twit.user.id
-					// twit.entities.hashtags
-					// twit.entities.urls
+					console.log(tweet)
+					let twitterID = tweet.user.id;
+					User.find({ "twitter.id": twitterID })
+					.populate('appletIds')
+					.exec(function(err, users) {
+						if (err) return done(err);
+						if (users.length !== 0) {
+							for (user of users) {
+								let applets = user.appletIds;
+								applets = applets.filter(applet => applet.option.watchFrom === 'Twitter' && applet.isActive);
+
+								// applets that triggered by any tweets 
+								anyTwitsApplets = applets.filter(applet => applet.trigger.twitterOptions.byAnyTweet  && applet.isActive);
+
+								// trello options / actions
+								let cardTitle = `[New tweet by ${tweet.user.name}]`;
+								let description = `[Text]: ${tweet.text}`;
+								trelloActions(anyTwitsApplets, user.trello.token, cardTitle, description);
+
+								//slack options / actions
+								let message = `[New tweet] Text: [${tweet.text}] Author: ${tweet.user.name}`;
+								slackActions(anyTwitsApplets, user.slack.token, message);
+
+								//mail actions
+								mailActions (anyTwitsApplets, message, user);
+
+								
+								//  applets that triggered by hasgtags in tweets 
+								let hashtags = tweet.entities.hashtags;
+								// checking if any applets have any hashtags from tweet as triggers
+								hashtagTwitsApplets = applets.filter(applet => hashtags.some(hashtag => hashtag.text === (applet.trigger.twitterOptions.hashtag) && applet.isActive));
+								// trello options / actions
+								cardTitle = `[New hashtaged tweet by ${tweet.user.name}]`;
+								description = `[Text]: ${tweet.text}`;
+								trelloActions(hashtagTwitsApplets, user.trello.token, cardTitle, description);
+
+								//slack options / actions
+								message = `[New hashtaged tweet] Text: [${tweet.text}] Author: ${tweet.user.name}`;
+								slackActions(hashtagTwitsApplets, user.slack.token, message);
+
+								//mail actions
+								mailActions (hashtagTwitsApplets, message, user);
+
+							}
+						}
+					})
 				 });
+
+				 stream.on('follow', function (follow) {
+					console.log(follow);
+					// tweet.source.id; tweet.target.name tweet.target.screen_name: // person who started following
+					// tweet.target.id  // person who was followed
+				 });			 
 			}
 			} else {
 			console.log("User not found");
 		}
+
+
+		
 	});
+
+
 		
 
 			
@@ -106,3 +202,4 @@ twitterRouter.get("/disconnect", isLoggedIn, (req, res, next) => {
 
 
 exports.twitterRouter = twitterRouter;
+exports.streams = streams;
--- src/Components/CreateApplet/CreateApplet.js
+++ src/Components/CreateApplet/CreateApplet.js
@@ -10,151 +10,152 @@ import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 //Main page in which create-applet happens
 class CreateApplet extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			currentStep: 1
-		};
-		this._changeStep = this._changeStep.bind(this);
-		this._next = this._next.bind(this);
-		this._doubleNext = this._doubleNext.bind(this);
-		this._setAction = this._setAction.bind(this);
-		this._finish = this._finish.bind(this);
-		this.modifyData = this.modifyData.bind(this);
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentStep: 1
+    };
+    this._changeStep = this._changeStep.bind(this);
+    this._next = this._next.bind(this);
+    this._doubleNext = this._doubleNext.bind(this);
+    this._setAction = this._setAction.bind(this);
+    this._finish = this._finish.bind(this);
+    this.modifyData = this.modifyData.bind(this);
+  }
 
-	//function describing what should happen when going to next page
-	_changeStep() {
-		let currentStep = this.state.currentStep;
-		if (currentStep >= 6) {
-			currentStep = 1;
-		} else {
-			currentStep = currentStep + 1;
-		}
-		this.setState({
-			currentStep: currentStep
-		});
-	}
+  //function describing what should happen when going to next page
+  _changeStep() {
+    let currentStep = this.state.currentStep;
+    if (currentStep >= 6) {
+      currentStep = 1;
+    } else {
+      currentStep = currentStep + 1;
+    }
+    this.setState({
+      currentStep: currentStep
+    });
+  }
 
-	_next(key, value) {
-		this._changeStep();
-		this.setState({
-			[key]: value
-		});
-	}
+  _next(key, value) {
+    this._changeStep();
+    this.setState({
+      [key]: value
+    });
+  }
 
-	_doubleNext(key1, value1, key2, value2, values = {}) {
-		this._changeStep();
-		this.setState({
-			[key1]: value1,
-			[key2]: value2,
-			...values
-		});
-	}
+  _doubleNext(key1, value1, key2, value2, values = {}) {
+    this._changeStep();
+    this.setState({
+      [key1]: value1,
+      [key2]: value2,
+      ...values
+    });
+  }
 
-	_setAction(obj) {
-		this._changeStep();
-		if (obj.field) {
-			this.setState({
-				heading: obj.field[0].heading,
-				content: obj.field[0].content
-			});
-		}
-		else if (this.state.serviceTo === 'Trello'){
-			this.setState({trelloOptions: obj})
-		}
-		else if (this.state.serviceTo === 'Slack'){
-			this.setState({slackOptions: obj})
-		}
-		else if (this.state.serviceTo === 'Mail'){
-			this.setState({mailOptions: obj})
-		}
-	}
+  _setAction(obj) {
+    this._changeStep();
+    if (obj.field) {
+      this.setState({
+        heading: obj.field[0].heading,
+        content: obj.field[0].content
+      });
+    } else if (this.state.serviceTo === "Trello") {
+      this.setState({ trelloOptions: obj });
+    } else if (this.state.serviceTo === "Slack") {
+      this.setState({ slackOptions: obj });
+    } else if (this.state.serviceTo === "Mail") {
+      this.setState({ mailOptions: obj });
+    }
+  }
 
-	modifyData(obj) {
-		let appletData = {
-			action: {
-				id: 4,
-				heading: obj.actionHeading,
-				content: obj.actionContent,
-				trelloOptions: obj.trelloOptions,
-				slackOptions: obj.slackOptions,
-				mailOptions: obj.mailOptions
-			},
-			trigger: {
-				id: 4,
-				heading: obj.triggerHeading,
-				content: obj.triggerContent,
-				twitterOptions: obj.twitterOptions
-			},
-			option: {
-				watchFrom: obj.serviceFrom,
-				watchTo: obj.serviceTo,
-				watchFor: "None"
-			},
-			// content: obj.content,
-			content: "If "+obj.triggerHeading+" then "+obj.actionHeading,
-			heading: obj.heading
-		};
-		return appletData;
-	}
+  modifyData(obj) {
+    let appletData = {
+      action: {
+        id: 4,
+        heading: obj.actionHeading,
+        content: obj.actionContent,
+        trelloOptions: obj.trelloOptions,
+        slackOptions: obj.slackOptions,
+        mailOptions: obj.mailOptions
+      },
+      trigger: {
+        id: 4,
+        heading: obj.triggerHeading,
+        content: obj.triggerContent,
+        twitterOptions: obj.twitterOptions
+      },
+      option: {
+        watchFrom: obj.serviceFrom,
+        watchTo: obj.serviceTo,
+        watchFor: "None"
+      },
+      // content: obj.content,
+      content: "If " + obj.triggerHeading + " then " + obj.actionHeading,
+      heading: obj.heading
+    };
+    return appletData;
+  }
 
-	_finish(obj) {
-		let appletData=this.modifyData(this.state);
-		this.props.createActions.addApplet(appletData);
-		this.setState({currentStep: 1})
-	}
+  _finish(obj) {
+    let appletData = this.modifyData(this.state);
+    this.props.createActions.addApplet(appletData);
+    this.setState({
+      currentStep: 1,
+      trelloOptions: undefined,
+      slackOptions: undefined,
+      twitterOptions: undefined
+    });
+  }
 
-	render() {
-		let currentStep = this.state.currentStep; //current step in create-applet
-		//returns the step which is the currentstep
-		//each step component, example, step1 checks if the current step matches with it, like here if current step is 1 then only step1 should be be returned or shown, and all else return null
-		return (
-			<div className="create-applet">
-				<div className="text-center">
-					<h1>Choose a service</h1>
-				</div>
-				<div className="text-center">
-					<p>Step {this.state.currentStep} of 6</p>
-				</div>
-				<Step1
-					currentStep={currentStep}
-					afterValid={this._next}
-					step="1"
-				/>
-				<Step2
-					currentStep={currentStep}
-					service={this.state.serviceFrom}
-					afterValid={this._doubleNext}
-					step="2"
-				/>
-				<Step3
-					currentStep={currentStep}
-					afterValid={this._next}
-					step="3"
-				/>
-				<Step4
-					currentStep={currentStep}
-					service={this.state.serviceTo}
-					afterValid={this._doubleNext}
-					step="4"
-					serviceFrom={this.state.serviceFrom}
-				/>
-				<Step5 currentStep={currentStep} afterValid={this._setAction} serviceTo={this.state.serviceTo}/>
-				<Step6
-					currentStep={currentStep}
-					data={this.state}
-					afterValid={this._finish}
-				/>
-			</div>
-		);
-	}
+  render() {
+    let currentStep = this.state.currentStep; //current step in create-applet
+    //returns the step which is the currentstep
+    //each step component, example, step1 checks if the current step matches with it, like here if current step is 1 then only step1 should be be returned or shown, and all else return null
+    return (
+      <div className="create-applet">
+        <div className="text-center">
+          <h1>Choose a service</h1>
+        </div>
+        <div className="text-center">
+          <p>Step {this.state.currentStep} of 6</p>
+        </div>
+        <Step1 currentStep={currentStep} afterValid={this._next} step="1" />
+        <Step2
+          currentStep={currentStep}
+          service={this.state.serviceFrom}
+          afterValid={this._doubleNext}
+          step="2"
+        />
+        <Step3 currentStep={currentStep} afterValid={this._next} step="3" />
+        <Step4
+          currentStep={currentStep}
+          service={this.state.serviceTo}
+          afterValid={this._doubleNext}
+          step="4"
+          serviceFrom={this.state.serviceFrom}
+        />
+        <Step5
+          currentStep={currentStep}
+          afterValid={this._setAction}
+          serviceTo={this.state.serviceTo}
+        />
+        <Step6
+          currentStep={currentStep}
+          data={this.state}
+          afterValid={this._finish}
+        />
+      </div>
+    );
+  }
 }
 
 const mapActionsToProps = dispatch => {
-	return {
-		createActions: bindActionCreators(createActions, dispatch)
-	};
+  return {
+    createActions: bindActionCreators(createActions, dispatch)
+  };
 };
 
-export default connect(null, mapActionsToProps)(CreateApplet);
+export default connect(
+  null,
+  mapActionsToProps
+)(CreateApplet);
