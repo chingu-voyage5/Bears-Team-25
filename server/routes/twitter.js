875c7c283ee400476b3e6f5aff2dad8cc799595e
Ruslan <4ruslan.k@gmail.com>
2018-07-09 19:08:46 +0500

    delete applets usin a service, when the service disconnected

--- server/routes/twitter.js
+++ server/routes/twitter.js
@@ -2,50 +2,61 @@ var Twit = require("twit");
 const express = require("express");
 const twitterRouter = express.Router();
 var passport = require("passport");
-var isLoggedIn = require('../commonFunctions').isLoggedIn;
+var isLoggedIn = require("../commonFunctions").isLoggedIn;
 var User = require("../models/users");
-var addToNotSubscribedRemoveFromSubscribed = require('../commonFunctions').addToNotSubscribedRemoveFromSubscribed;
 var postTrelloCard = require("../routes/trello").postCard;
 var slackSendMessage = require("../routes/slack").slackSendMessage;
-var transporter = require('../routes/email').transporter;
-var mailOptions = require('../routes/email').mailOptions;
+var transporter = require("../routes/email").transporter;
+var mailOptions = require("../routes/email").mailOptions;
+var deleteApplets = require('../commonFunctions').deleteApplets;
 var consumer_key = JSON.parse(process.env.twitter).consumerKey;
 var consumer_secret = JSON.parse(process.env.twitter).consumerSecret;
 
-
-function trelloActions (applets, trelloToken, cardTitle, description) {
-	appletsWithTrelloActions = applets.filter(applet => applet.option.watchTo === 'Trello');
-	for (appletsWithTrelloAction of appletsWithTrelloActions) {
-		let trelloOptions = appletsWithTrelloAction.action.trelloOptions;
-		let trelloConfig = {...trelloOptions, token: trelloToken, cardTitle, description}
-		postTrelloCard(trelloConfig).then(card => {
-			console.log("trello card posted");
-		});
-	}
+function trelloActions(applets, trelloToken, cardTitle, description) {
+  appletsWithTrelloActions = applets.filter(
+    applet => applet.option.watchTo === "Trello"
+  );
+  for (appletsWithTrelloAction of appletsWithTrelloActions) {
+    let trelloOptions = appletsWithTrelloAction.action.trelloOptions;
+    let trelloConfig = {
+      ...trelloOptions,
+      token: trelloToken,
+      cardTitle,
+      description
+    };
+    postTrelloCard(trelloConfig).then(card => {
+      console.log("trello card posted");
+    });
+  }
 }
 
 function slackActions(applets, slackToken, message) {
-	appletsWithSlackActions = applets.filter(applet => applet.option.watchTo === 'Slack');
-	// console.log(appletsWithSlackActions)
-	for (appletsWithSlackAction of appletsWithSlackActions) {
-		let slackOptions = appletsWithSlackAction.action.slackOptions;
-		slackSendMessage(slackToken, message, slackOptions.to).then(message => {
-			console.log("slack message sent");
-		});
-	}
+  appletsWithSlackActions = applets.filter(
+    applet => applet.option.watchTo === "Slack"
+  );
+  // console.log(appletsWithSlackActions)
+  for (appletsWithSlackAction of appletsWithSlackActions) {
+    let slackOptions = appletsWithSlackAction.action.slackOptions;
+    slackSendMessage(slackToken, message, slackOptions.to).then(message => {
+      console.log("slack message sent");
+    });
+  }
 }
 
-function mailActions (applets, message, user) {
-	appletsWithMailActions = applets.filter(applet => applet.option.watchTo === 'Mail');
-	for (appletsWithMailAction of appletsWithMailActions) {
-		let options = mailOptions(user, {email:  appletsWithMailAction.action.mailOptions.email,
-				message: message})
-		transporter.sendMail(options).then(message => {
-			console.log("email  sent");
-		});
-	}
+function mailActions(applets, message, user) {
+  appletsWithMailActions = applets.filter(
+    applet => applet.option.watchTo === "Mail"
+  );
+  for (appletsWithMailAction of appletsWithMailActions) {
+    let options = mailOptions(user, {
+      email: appletsWithMailAction.action.mailOptions.email,
+      message: message
+    });
+    transporter.sendMail(options).then(message => {
+      console.log("email  sent");
+    });
+  }
 }
-
 
 twitterRouter.get("/auth", passport.authorize("twitter"));
 
@@ -54,140 +65,154 @@ twitterRouter.get(
   passport.authenticate("twitter", {
     failureRedirect: "http://localhost:3000/error/Something went wrong."
   }),
-  (req, res) => res.redirect("http://localhost:3000/success/Twitter successfully connected.") // Successful authentication, redirect home.
+  (req, res) =>
+    res.redirect(
+      "http://localhost:3000/success/Twitter successfully connected."
+    ) // Successful authentication, redirect home.
 );
 
 twitterRouter.get("/disconnect", isLoggedIn, (req, res, next) => {
-	var user = req.user;
-	user.slack = undefined;
-	addToNotSubscribedRemoveFromSubscribed('Twitter', user.servicesSubscribed, user.servicesNotSubscribed);
-	user.save().then(
-	  (user) => {
-		res.statusCode = 200;
-		res.setHeader("Content-Type", "application/json");
-		const {servicesNotSubscribed, servicesSubscribed} = user;
-		res.json({ servicesNotSubscribed, servicesSubscribed });
-		return;
-	  },
-	  err => {
-		console.log(err);
-		return next(err);
-	  }
-	);
-	});
-	
-	var streams = {}
+  var user = req.user;
+  user.twitter = undefined;
+  deleteApplets('Twitter', user, res, next);
+});
 
-	User.find({ 'twitter.id': { $exists: true } })
-	.exec(function(err, users) {
-		if (err) return done(err);
-		if (users.length !== 0) {
-			for (let user of users) {
-				openStreamsForTwitterUser(user, streams)
-			}
-		} else {
-			console.log("User not found");
-		}	
-	});
+// launching streams for each user that have twitter account connected
 
+var streams = {};
 
-	function openStreamsForTwitterUser(user, streams) {
-		let id = user.twitter.id;
-		if (streams[id]) return
-		var T = new Twit({
-			consumer_key,
-			consumer_secret,
-			access_token: user.twitter.token,
-			access_token_secret: user.twitter.tokenSecret,
-			timeout_ms: 60 * 1000, // optional HTTP request timeout to apply to all requests.
-		});
-		
+User.find({ "twitter.id": { $exists: true } }).exec(function(err, users) {
+  if (err) return done(err);
+  if (users.length !== 0) {
+    for (let user of users) {
+      openStreamsForTwitterUser(user, streams);
+    }
+  } else {
+    console.log("User not found");
+  }
+});
 
-		var stream = T.stream('user');
-		streams[id] = stream;
-		console.log(id)
-		stream.on('tweet', function (tweet) {
-			// console.log(tweet)
-			let twitterID = tweet.user.id;
-			console.log('twitterID', twitterID)
-			User.find({ "twitter.id": twitterID })
-			.populate('appletIds')
-			.exec(function(err, users) {
-				if (err) return done(err);
-				if (users.length !== 0) {
-					for (user of users) {
-						let applets = user.appletIds;
-						applets = applets.filter(applet => applet.option.watchFrom === 'Twitter' && applet.isActive);
+function openStreamsForTwitterUser(user, streams) {
+  let id = user.twitter.id;
+  if (streams[id]) return;
+  var T = new Twit({
+    consumer_key,
+    consumer_secret,
+    access_token: user.twitter.token,
+    access_token_secret: user.twitter.tokenSecret,
+    timeout_ms: 60 * 1000 // optional HTTP request timeout to apply to all requests.
+  });
 
-						// applets that triggered by any tweets 
-						anyTwitsApplets = applets.filter(applet => applet.trigger.twitterOptions.byAnyTweet  && applet.isActive);
+  var stream = T.stream("user");
+  streams[id] = stream;
+ // console.log(id);
+  stream.on("tweet", function(tweet) {
+    // console.log(tweet)
+    let twitterID = tweet.user.id;
+   // console.log("twitterID", twitterID);
+    User.find({ "twitter.id": twitterID })
+      .populate("appletIds")
+      .exec(function(err, users) {
+        if (err) return done(err);
+        if (users.length !== 0) {
+          for (user of users) {
+            let applets = user.appletIds;
+            applets = applets.filter(
+              applet => applet.option.watchFrom === "Twitter" && applet.isActive
+            );
 
-						// trello options / actions
-						let cardTitle = `[New tweet by ${tweet.user.name}]`;
-						let description = `Text: ${tweet.text}`;
-						trelloActions(anyTwitsApplets, user.trello.token, cardTitle, description);
+            // applets that triggered by any tweets
+            anyTwitsApplets = applets.filter(
+              applet =>
+                applet.trigger.twitterOptions.byAnyTweet && applet.isActive
+            );
 
-						//slack options / actions
-						let message = `[New tweet] Text: ${tweet.text} Author: ${tweet.user.name}`;
-						slackActions(anyTwitsApplets, user.slack.token, message);
+            // trello options / actions
+            let cardTitle = `[New tweet by ${tweet.user.name}]`;
+            let description = `Text: ${tweet.text}`;
+            trelloActions(
+              anyTwitsApplets,
+              user.trello.token,
+              cardTitle,
+              description
+            );
 
-						//mail actions
-						mailActions (anyTwitsApplets, message, user);
+            //slack options / actions
+            let message = `[New tweet] Text: ${tweet.text} Author: ${
+              tweet.user.name
+            }`;
+            slackActions(anyTwitsApplets, user.slack.token, message);
 
-						
-						//  applets that triggered by hasgtags in tweets 
-						let hashtags = tweet.entities.hashtags;
-						// checking if any applets have any hashtags from tweet as triggers
-						hashtagTwitsApplets = applets.filter(applet => hashtags.some(hashtag => hashtag.text === (applet.trigger.twitterOptions.hashtag) && applet.isActive));
-						// trello options / actions
-						cardTitle = `[New hashtaged tweet by ${tweet.user.name}]`;
-						description = `[Text]: ${tweet.text}`;
-						trelloActions(hashtagTwitsApplets, user.trello.token, cardTitle, description);
+            //mail actions
+            mailActions(anyTwitsApplets, message, user);
 
-						//slack options / actions
-						message = `[New hashtaged tweet] Text: [${tweet.text}] Author: ${tweet.user.name}`;
-						slackActions(hashtagTwitsApplets, user.slack.token, message);
+            //  applets that triggered by hasgtags in tweets
+            let hashtags = tweet.entities.hashtags;
+            // checking if any applets have any hashtags from tweet as triggers
+            hashtagTwitsApplets = applets.filter(applet =>
+              hashtags.some(
+                hashtag =>
+                  hashtag.text === applet.trigger.twitterOptions.hashtag &&
+                  applet.isActive
+              )
+            );
+            // trello options / actions
+            cardTitle = `[New hashtaged tweet by ${tweet.user.name}]`;
+            description = `[Text]: ${tweet.text}`;
+            trelloActions(
+              hashtagTwitsApplets,
+              user.trello.token,
+              cardTitle,
+              description
+            );
 
-						//mail actions
-						mailActions (hashtagTwitsApplets, message, user);
+            //slack options / actions
+            message = `[New hashtaged tweet] Text: [${tweet.text}] Author: ${
+              tweet.user.name
+            }`;
+            slackActions(hashtagTwitsApplets, user.slack.token, message);
 
-					}
-				}
-			})
-	 });
+            //mail actions
+            mailActions(hashtagTwitsApplets, message, user);
+          }
+        }
+      });
+  });
 
-	 stream.on('follow', function (followAction) {
-		let personWhoFollows = followAction.source.name;
-		let personWhoisFollowed = followAction.target.name;
-		let twitterID = followAction.target.id;
-		User.find({ "twitter.id": twitterID })
-		.populate('appletIds')
-		.exec(function(err, users) {
-			if (err) return done(err);
-			if (users.length !== 0) {
-				for (user of users) {
-					let applets = user.appletIds;
-					applets = applets.filter(applet => applet.option.watchFrom === 'Twitter' 
-					&& applet.trigger.twitterOptions.byNewFollower && applet.isActive );
+  stream.on("follow", function(followAction) {
+    let personWhoFollows = followAction.source.name;
+    let personWhoisFollowed = followAction.target.name;
+    let twitterID = followAction.target.id;
+    User.find({ "twitter.id": twitterID })
+      .populate("appletIds")
+      .exec(function(err, users) {
+        if (err) return done(err);
+        if (users.length !== 0) {
+          for (user of users) {
+            let applets = user.appletIds;
+            applets = applets.filter(
+              applet =>
+                applet.option.watchFrom === "Twitter" &&
+                applet.trigger.twitterOptions.byNewFollower &&
+                applet.isActive
+            );
 
-					// trello options / actions
-					let cardTitle = `[New ${personWhoisFollowed}'s follower]`;
-					let description = `${personWhoFollows} started following ${personWhoisFollowed}`;
-					trelloActions(applets, user.trello.token, cardTitle, description);
+            // trello options / actions
+            let cardTitle = `[New ${personWhoisFollowed}'s follower]`;
+            let description = `${personWhoFollows} started following ${personWhoisFollowed}`;
+            trelloActions(applets, user.trello.token, cardTitle, description);
 
-					//slack options / actions
-					let message = `${personWhoFollows} started following ${personWhoisFollowed}`;
-					slackActions(applets, user.slack.token, message);
+            //slack options / actions
+            let message = `${personWhoFollows} started following ${personWhoisFollowed}`;
+            slackActions(applets, user.slack.token, message);
 
-					//mail actions
-					mailActions (applets, message, user);
-				}
-			}
-		})
-	 });
-
-	}
-
+            //mail actions
+            mailActions(applets, message, user);
+          }
+        }
+      });
+  });
+}
 
 exports.twitterRouter = twitterRouter;
 exports.streams = streams;
